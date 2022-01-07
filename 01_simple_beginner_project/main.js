80ef46086e988e64228b1110c5755a44f74bb356
habibullahturkmen <habibullah_turkmen@yahoo.con>
2022-01-07 14:53:03 +0430

    vue instance, binding, conditionals

---
+++ 01_simple_beginner_project/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm, and fuzzy socks',
+        image: 'assets/vmSocks-green-onWhite.jpg',
+        inStock: true,
+        details: ["80% cotton", "20% polyester", "Gender-Neutral"]
+    }
+});
+
